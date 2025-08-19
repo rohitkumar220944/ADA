63343502df1cdd7b657b1da1d1aad958cfead6a0
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2025-08-20 02:03:36 +0530

    Add files via upload

---
+++ sce.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+void knapsack(int n, float weight[], float profit[], float capacity)
+{
+   float x[20], tp = 0;
+   int i, j, u;
+   u = capacity;
+
+   for (i = 0; i < n; i++)
+      x[i] = 0.0;
+
+   for (i = 0; i < n; i++) {
+      if (weight[i] > u)
+         break;
+      else {
+         x[i] = 1.0;
+         tp = tp + profit[i];
+         u = u - weight[i];
+      }
+   }
+
+   if (i < n)
+      x[i] = u / weight[i];
+
+   tp = tp + (x[i] * profit[i]);
+
+   printf("\nThe result vector is:- ");
+   for (i = 0; i < n; i++)
+      printf("%f\t", x[i]);
+
+   printf("\nMaximum profit is:- %f", tp);
+
+}
+
+int main() {
+   float weight[20], profit[20], capacity;
+   int num, i, j;
+   float ratio[20], temp;
+   printf("Name:-Rohit kumar\nEnrollment number:-0827CS221232\nTopic:-Code to implement Fractional knapsack problem.");
+   printf("\nEnter the no. of objects:- ");
+   scanf("%d", &num);
+
+   printf("\nEnter the weights and profits of each object:\n");
+   for(i=0;i<num;i++){
+printf("Enter weight of %d object:-",i);
+scanf("%f",&weight[i]);
+}
+printf("\n");
+for(i=0;i<num;i++){
+printf("Enter profit of %d object:-",i);
+scanf("%f",&profit[i]);
+}
+   
+
+   printf("\nEnter the capacity of knapsack:- ");
+   scanf("%f", &capacity);
+
+   for (i = 0; i < num; i++) {
+      ratio[i] = profit[i] / weight[i];
+   }
+
+   for (i = 0; i < num; i++) {
+      for (j = i + 1; j < num; j++) {
+         if (ratio[i] < ratio[j]) {
+            temp = ratio[j];
+            ratio[j] = ratio[i];
+            ratio[i] = temp;
+
+            temp = weight[j];
+            weight[j] = weight[i];
+            weight[i] = temp;
+
+            temp = profit[j];
+            profit[j] = profit[i];
+            profit[i] = temp;
+         }
+      }
+   }
+
+   knapsack(num, weight, profit, capacity);
+   return(0);
+}
